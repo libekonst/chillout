6669d35840f6fae7eb99573e68050a4d1b8e0096
Kostas Liberopoulos <k.liberopoulos@outlook.com>
2018-06-15 02:54:51 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const x = document.getElementById('offRadio');
+
+function playAudio(){
+    document.getElementById('offRadio').play();
+}
+
+function pauseAudio(){
+    document.getElementById('offRadio').pause();
+}
