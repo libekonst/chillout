e2478308dac3166038165264310e8faff98d5780
Kostas Liberopoulos <k.liberopoulos@outlook.com>
2018-06-15 18:33:17 +0300

    working on accessing the Audio object

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-const x = document.getElementById('offRadio');
+// const audio = document.getElementById('radioSource');
+// const player = new Audio("http://46.28.53.118:7062/stream?1529011397134");
+let audio = 'http://46.28.53.118:7062/stream?1529011397134';
+
+
+/* 
+function audioInitializer(source){
+    const player = new Audio(source);
+}
+ */
+
+let url = 'http://46.28.53.118:7062/stream?1529011397134';
+const player = new Audio(url);
+// audioInitializer(url);
 
 function playAudio(){
-    document.getElementById('offRadio').play();
+    player.play();
 }
 
 function pauseAudio(){
-    document.getElementById('offRadio').pause();
+    // alert('the function works');
+    player.pause();
 }
