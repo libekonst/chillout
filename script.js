d49019e874ff3619c534e1046227d7e0cc9119e2
Kostas Liberopoulos <k.liberopoulos@outlook.com>
2018-06-15 18:38:50 +0300

    audio

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-// const audio = document.getElementById('radioSource');
-// const player = new Audio("http://46.28.53.118:7062/stream?1529011397134");
-let audio = 'http://46.28.53.118:7062/stream?1529011397134';
-
-
 /* 
 function audioInitializer(source){
     const player = new Audio(source);
-}
- */
+}*/
 
+// using audio as the argument on the constructor doesn't work.
+// audio is an object. Can I / Do I need to access the url itself from the source tag? 
+let audio = document.getElementById('radioSource');
 let url = 'http://46.28.53.118:7062/stream?1529011397134';
 const player = new Audio(url);
 // audioInitializer(url);
