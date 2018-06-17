94298a7dbc0e913b2a2a83019229cc083441906c
Kostas Liberopoulos <k.liberopoulos@outlook.com>
2018-06-17 19:56:58 +0300

    working on functions

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-/* // using audio as the argument on the constructor doesn't work.
-// audio is an object. Can I / Do I need to access the url itself from the source tag? 
-let audio = document.getElementById('radioSource');
-let url = 'http://46.28.53.118:7062/stream?1529011397134';
- */
 //@ts-check
+
 const radios = {
     offradio: 'http://46.28.53.118:7062/stream?1529011397134',
     enlefko: 'http://stream.radiojar.com/enlefko877',
@@ -11,23 +7,34 @@ const radios = {
     skai: 'http://liveradio.skai.gr/skaihd/skai/playlist.m3u8'
 };
 
-let currentRadio = '';
 const player = new Audio();
+let isPlaying = false;
+let currentRadio = '';
+
+let playButton = document.getElementById('playButton');
 
 function playAudio(source){
-    player.src = source;
-    player.play();
-    currentRadio = source;
+    if (player.src !== source) {
+        player.src = source;
+        player.play();
+        isPlaying = true;
+        currentRadio = source;
+        playButton.innerHTML = 'Pause';
+    } else if (isPlaying) {
+        player.pause();
+        isPlaying = false;
+        playButton.innerHTML = 'Play!';
+    } else {
+        player.play();
+        isPlaying = true;
+        playButton.innerHTML = 'Pause';
+    }
 }
 
-function playButton(){
-    // player.muted = false;
-    player.play();
-
-    
-}
-
-function pauseAudio(){
-    // player.muted = true;
-    player.pause();
+function buttonText(){
+    if (currentRadio === ''){
+        alert('Click an image to load a radio!');
+    } else {
+        playAudio(currentRadio);
+    }
 }
