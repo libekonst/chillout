d5700216616a6ae105db3dd4e95d0c3c4c03faf3
Kostas Liberopoulos <k.liberopoulos@outlook.com>
2018-06-25 03:37:31 +0300

    Polishing styles

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
-// Try adding support for mobile: onmouseover == tap, onmousedown == second tap.
+// Try to add support for mobile: onmouseover == tap, onmousedown == second tap.
 
 //@ts-check
 
 let offradio = document.getElementById('offradio');
-let one = document.getElementById('1');
-let two = document.getElementById('2');
-let three = document.getElementById('3');
-let four = document.getElementById('4');
-let five = document.getElementById('5');
-let six = document.getElementById('6');
-let seven = document.getElementById('7');
+
 function changeToHover(){
     if(offradio.className != "radioItemTest radioItemActive"){
         offradio.className = "radioItemTest radioItemHover";
