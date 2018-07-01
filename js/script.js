700e2579a90757d71c989910452ffcf39da34881
Kostas Liberopoulos <k.liberopoulos@outlook.com>
2018-07-02 02:33:12 +0300

    Comments.

--- js/script.js
+++ js/script.js
@@ -2,12 +2,12 @@
 
 // Creates an object that holds class names used by the event handlers.
 class AnimationState {
-    constructor(name='') {
+    constructor(name = '') {
         this.className = name;
         this.active = `${name} ${name}-active`;
         this.hover = `${name} ${name}-hover`;
         this.idle = name;
-    }    
+    }
 }
 
 // Contains static methods that can be used as event handlers.
@@ -19,15 +19,13 @@ class BasicAnimations {
     }
 
     static changeToActive(radio = {}) {
+        // Change other active items (normally 1) to idle, then change current item to active.
         if (radio.className !== testState.active) {
-            // Change other active items (normally 1) to idle, then change current item to active.
-
             let otherActive = document.getElementsByClassName(testState.active); // HTMLCollection, indexable array-like object.
-            for (let i = 0; i < otherActive.length; i++) {  // Fail safe. Could just use otherActive[0].
+            for (let i = 0; i < otherActive.length; i++) { // Fail safe. Could just use otherActive[0].
                 otherActive[i].className = testState.idle;
             }
             radio.className = testState.active;
-
         } else {
             radio.className = testState.hover;
         }
@@ -37,18 +35,18 @@ class BasicAnimations {
         if (radio.className !== testState.active) {
             radio.className = testState.idle;
         }
-    } 
+    }
 }
 
-let radioIDs = ['offradio', 'best', 'enlefko', 'imagine', 'pepper'];  // Fn that parses json and returns array?
-let testState = new AnimationState('radio-item');  // Temporary solution to give the object the specific name. Need a way to avoid initializing it this way.
+let radioIDs = ['offradio', 'best', 'enlefko', 'imagine', 'pepper']; // Fn that parses json and returns array?
+let testState = new AnimationState('radio-item'); // Gives the object the specific name. Temporary.
 animationInitializer(radioIDs);
 
 
 // Calls addEventHandlers() for each array element (i.e. event target).
-function animationInitializer(arrayOfIDs) {    
+function animationInitializer(arrayOfIDs) {
     if (!Array.isArray(arrayOfIDs)) {
-        console.error('Parameter must be an array');  // Ensures the parameter is an array.
+        console.error('Parameter must be an array'); // Ensures the parameter is an array.
     } else {
         arrayOfIDs.forEach(addEventHandlers);
     }
@@ -58,18 +56,24 @@ function animationInitializer(arrayOfIDs) {
 
 // A callback fn. Adds event handlers to the event target.
 function addEventHandlers(radioID) {
-    
+
     const radio = document.getElementById(radioID); // Returns an Element object. The object already exists in the DOM.
 
-    radio.onmousedown = () => { BasicAnimations.changeToActive(radio); };
-    radio.onmouseover = () => { BasicAnimations.changeToHover(radio); };
-    radio.onmouseout = () => { BasicAnimations.resetClassName(radio); };
-/**
-  * Javascript finds the declarations for the functions referenced above.
-  * The object's properties are updated.
-  * After addEvenetHandlers() finishes executing, the reference to the object is lost and freed up.
-  * As a result, the variable 'radio' can be reassigned and this allows the fn to be used as a callback repeatedly by animationInitializer().
-  */
+    radio.onmousedown = () => {
+        BasicAnimations.changeToActive(radio);
+    };
+    radio.onmouseover = () => {
+        BasicAnimations.changeToHover(radio);
+    };
+    radio.onmouseout = () => {
+        BasicAnimations.resetClassName(radio);
+    };
+    /**
+     * Javascript finds the declarations for the functions referenced above.
+     * The object's properties are updated.
+     * After addEvenetHandlers() finishes executing, the reference to the object is lost and freed up.
+     * As a result, the variable 'radio' can be reassigned and this allows the fn to be used as a callback repeatedly by animationInitializer().
+     */
 }
 
 
