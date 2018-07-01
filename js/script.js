efbee386f4d5494017ce4628ca60210f176aa92d
Kostas Liberopoulos <k.liberopoulos@outlook.com>
2018-07-02 01:25:54 +0300

    Organized animation functions as static methods of class BasicAnimations

--- js/script.js
+++ js/script.js
@@ -9,8 +9,37 @@ class AnimationState {
     }    
 }
 
+class BasicAnimations {
+    static changeToHover(radio = {}) {
+        if (radio.className !== testState.active) {
+            radio.className = testState.hover;
+        }
+    }
+
+    static changeToActive(radio = {}) {
+        if (radio.className !== testState.active) {
+            // Change other active items (normally 1) to idle, then change current item to active.
+
+            let otherActive = document.getElementsByClassName(testState.active); // HTMLCollection, indexable array-like object.
+            for (let i = 0; i < otherActive.length; i++) {  // Fail safe. Could just use otherActive[0].
+                otherActive[i].className = testState.idle;
+            }
+            radio.className = testState.active;
+
+        } else {
+            radio.className = testState.hover;
+        }
+    }
+
+    static resetClassName(radio = {}) {
+        if (radio.className !== testState.active) {
+            radio.className = testState.idle;
+        }
+    } 
+}
 
 let radioIDs = ['offradio', 'best', 'enlefko', 'imagine', 'pepper'];  // Fn that parses json and returns array?
+let testState = new AnimationState('radio-item');  // Temporary solution to give the object the specific name. Need a way to avoid initializing it this way.
 animationInitializer(radioIDs);
 
 
@@ -23,50 +52,23 @@ function animationInitializer(arrayOfIDs) {
     }
 }
 
+
+
 // A callback fn. Adds event handlers to the event target.
 function addEventHandlers(radioID) {
-    const radio = document.getElementById(radioID); // Returns an Element object. This object already exists in the DOM.
+    
+    const radio = document.getElementById(radioID); // Returns an Element object. The object already exists in the DOM.
 
-    radio.onmousedown = () => { changeToActive(radio); };
-    radio.onmouseover = () => { changeToHover(radio); };
-    radio.onmouseout = () => { resetClassName(radio); };
-
-    // 1. Javascript finds the declarations for the functions referenced above.
-    // 2. The object's properties are updated.
-    // 3. After addEvenetHandlers() finishes executing, the reference to the object is lost and freed up.
-    // 4. As a result, the variable 'radio' can be reassigned and this allows the fn to be used as a callback repeatedly.
+    radio.onmousedown = () => { BasicAnimations.changeToActive(radio); };
+    radio.onmouseover = () => { BasicAnimations.changeToHover(radio); };
+    radio.onmouseout = () => { BasicAnimations.resetClassName(radio); };
+/**
+  * Javascript finds the declarations for the functions referenced in above.
+  * The object's properties are updated.
+  * After addEvenetHandlers() finishes executing, the reference to the object is lost and freed up.
+  * As a result, the variable 'radio' can be reassigned and this allows the fn to be used as a callback repeatedly.
+  */
 }
-
-
-let testState = new AnimationState('radio-item');
-
-function changeToHover(radio = {}) {
-    if (radio.className !== testState.active) {
-        radio.className = testState.hover;
-    }
-}
-
-function changeToActive(radio = {}) {
-    if (radio.className !== testState.active) {
-        // Change other active items (normally 1) to idle, then change current item to active.
-
-        let otherActive = document.getElementsByClassName(testState.active); // HTMLCollection, indexable array-like object.
-        for (let i = 0; i < otherActive.length; i++) {  // Fail safe. Could just use otherActive[0].
-            otherActive[i].className = testState.idle;
-        }
-        radio.className = testState.active;
-
-    } else {
-        radio.className = testState.hover;
-    }
-}
-
-function resetClassName(radio = {}) {
-    if (radio.className !== testState.active) {
-        radio.className = testState.idle;
-    }
-} 
-
 
 
 
