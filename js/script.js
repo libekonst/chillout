b9b4d845b2244d3b288d834a7b5f1bba5c1244bc
Kostas Liberopoulos <k.liberopoulos@outlook.com>
2018-07-01 21:01:04 +0300

    Cleaned up animationHandler.

--- js/script.js
+++ js/script.js
@@ -3,32 +3,46 @@
 let radioID = ['offradio', 'best', 'enlefko', 'imagine', 'pepper'];
 animationInitializer(radioID);
 
-function animationHandler (radio) {
+function animationInitializer(arrayOfIDs) {
+    if (!Array.isArray(arrayOfIDs)) {
+        console.error('Parameter must be an array');
+    } else {
+        arrayOfIDs.forEach(animationHandler);
+    }
+}
+
+function animationHandler(radio) {
     // A function responsible for listening to mouse events and responding with the proper animation
 
-    radio = document.getElementById(radio);  // returns object
-   
-    function changeToHover(){
-        if(radio.className != "radio-item radio-item-active"){
-            radio.className = "radio-item radio-item-hover";
+    radio = document.getElementById(radio); // returns an Element object
+    let active = 'radio-item radio-item-active';
+    let hover = 'radio-item radio-item-hover';
+    let idle = 'radio-item';
+
+    function changeToHover() {
+        if (radio.className != active) {
+            radio.className = hover;
         }
     }
-    
-    function changeToActive(){
-        if(radio.className != "radio-item radio-item-active"){
-            let otherActive = document.getElementsByClassName('radio-item radio-item-active');  // returns an indexable NodeList object 
-            for (let index = 0; index < otherActive.length; index++) {
-                otherActive[index].className = 'radio-item';                
+
+    function changeToActive() {
+        if (radio.className != active) {
+            // Change other active items (normally 1) to idle, then change current item to active.
+
+            let otherActive = document.getElementsByClassName(active); // HTMLCollection, indexable array-like object.
+            for (let i = 0; i < otherActive.length; i++) {
+                otherActive[i].className = idle;
             }
-            radio.className = "radio-item radio-item-active";
+            radio.className = active;
+
         } else {
-            radio.className = "radio-item radio-item-hover";
+            radio.className = hover;
         }
     }
-    
-    function resetClassName(){
-        if(radio.className != "radio-item radio-item-active"){
-            radio.className = "radio-item";
+
+    function resetClassName() {
+        if (radio.className != active) {
+            radio.className = idle;
         }
     }
 
@@ -38,13 +52,7 @@ function animationHandler (radio) {
 }
 
 
-function animationInitializer(radioArray){
-    if (!Array.isArray(radioArray)) {
-        console.error('Parameter must be an array');
-    } else {
-        radioArray.forEach(animationHandler);
-    }
-}
+
 
 
 
@@ -54,25 +62,25 @@ function animationInitializer(radioArray){
 function playButtonAnimation() {
     const playImage = document.getElementById('play-image');
 
-    function changeToHover(){
-        if(playImage.className == "play-image"){
+    function changeToHover() {
+        if (playImage.className == "play-image") {
             playImage.className = "play-image play-image-hover";
         } else {
             playImage.className = "play-image play-image-active-hover";
         }
     }
-    
-    
-    function changeToActive(){
-        if(playImage.className == "play-image play-image-hover"|| playImage.className == "play-image"){
+
+
+    function changeToActive() {
+        if (playImage.className == "play-image play-image-hover" || playImage.className == "play-image") {
             playImage.className = "play-image play-image-active";
         } else {
             playImage.className = "play-image play-image-hover";
         }
     }
-    
-    function resetClassName(){
-        if(playImage.className == "play-image play-image-hover"){
+
+    function resetClassName() {
+        if (playImage.className == "play-image play-image-hover") {
             playImage.className = "play-image";
         } else if (playImage.className == "play-image play-image-active-hover") {
             playImage.className = "play-image play-image-active";
