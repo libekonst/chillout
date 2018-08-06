beb12ad3f343df35356325cbec0ddd4e5c270933
Kostas Liberopoulos <k.liberopoulos@outlook.com>
2018-08-06 06:26:23 +0300

    Moved requestJSON fn to separate module.

---
+++ src/js/requestJSON.js
@@ -0,0 +1,22 @@
+import { RadioItem } from "./populateDOM";
+
+//@ts-check
+export function requestJSON() {
+    const requestURL = 'http://127.0.0.1:5500/src/radios.json';
+    const request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
+    request.onload = () => {
+        const radioData = request.response;
+        console.log(radioData);
+        for (let radio of radioData.music) {
+            console.log(radio);
+            new RadioItem(radio.name, radio.id, radio.img, 'music');
+        }
+        for (const radio of radioData.news) {
+            console.log(radio);
+            new RadioItem(radio.name, radio.id, radio.img, 'news');
+        }
+    };
+}
