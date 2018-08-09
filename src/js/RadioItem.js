d0dee44b8aace3fc73911cf68a287026f3e05ebf
Kostas Liberopoulos <k.liberopoulos@outlook.com>
2018-08-09 07:02:19 +0300

    Comments.

--- src/js/RadioItem.js
+++ src/js/RadioItem.js
@@ -2,11 +2,11 @@
 export class RadioItem{
 
     /** 
-     * Bundles properties and attributes together to create a radio item, load it to the DOM and assign event listeners to it.
-     * @param {String} name 
-     * @param {String} id 
-     * @param {String} imgURL 
-     * @param {String} type 
+     * Bundles properties together to create a radio item, load it to the DOM and assign event listeners to it.
+     * @param {String} name The value used as the "data-name" attribute.
+     * @param {String} id The value used as the "id" attribute.
+     * @param {String} imgURL The source for the background image of the child div.
+     * @param {String} type Used to render the radio item under a certain parent <ul> element, based on its place in the JSON file.
      */
     constructor(name, id, imgURL, type){
         this.name = name;
