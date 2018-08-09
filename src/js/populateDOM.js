c9a3cc78572e6f54cdd930660ffe36fbe752601d
Kostas Liberopoulos <k.liberopoulos@outlook.com>
2018-08-09 22:18:24 +0300

    Comments.

--- src/js/populateDOM.js
+++ src/js/populateDOM.js
@@ -1,8 +1,13 @@
 import { RadioItem } from "./RadioItem";
 
 //@ts-check
+/**
+ * Sends an HTTP Request to get the radios.json file.
+ * When the response property is implemented with the response object, 
+ * call a function that handles the json data from the response.
+ */
 export function requestJSON() {
-    const requestURL = 'https://kostaslib.github.io/chillout/src/radios.json';
+    const requestURL = '../src/radios.json';
     const request = new XMLHttpRequest();
     request.open('GET', requestURL);
     request.responseType = 'json';
@@ -25,15 +30,19 @@ function renderRadioSection(request){
     }
 }
 
+/**
+ * Parses the response object and creates RadioItem objects using its data.
+ * Each RadioItem object loads itself to the DOM when it gets created.
+ * @param {XMLHttpRequest} request A XMLHttpRequest object which has a response property.
+ */
 function renderRadios(request) {
     const radioData = request.response;
     console.log(`Radio data: ${radioData}`);
     for (let radio of radioData.music) {
-        console.log(radio);
-        new RadioItem(radio.name, radio.id, radio.img, 'music');
+        // The created object is not stored in a var because we don't need it anymore. We can access the Element directly from the DOM.
+        new RadioItem(radio.name, radio.id, radio.img, radio.source, 'music');
     }
     for (const radio of radioData.news) {
-        console.log(radio);
-        new RadioItem(radio.name, radio.id, radio.img, 'news');
+        new RadioItem(radio.name, radio.id, radio.img, radio.source, 'news');
     }
 }
