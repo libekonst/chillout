7a00891fdc24d3f879eb25935bc4c217c42cdad8
KostasLib <k.liberopoulos@outlook.com>
2018-09-10 00:06:03 +0300

    Cleanup.

--- src/js/helper.js
+++ src/js/helper.js
@@ -1,29 +1,24 @@
 //@ts-check
 
 /**
- * Retrieves an HTMLCollection of Elements by their className and returns an array of each Element's requested attribute.
+ * Finds Elements by their className and returns an array of each Element's requested attribute.
  * @param {string} className The class name by which Elements will be collected.
  * @param {string} attr The requested attribute.
  * @returns {Array<string>} An array of attribute values as strings.
  */
 export function getAttributeByClassName(className, attr) {
-    const radioCollection = document.getElementsByClassName(className);
-    let arrayOfAttributes = [];
-    
-    validateRequest();
-    for (let radio of radioCollection) {
-        arrayOfAttributes.push(radio.getAttribute(attr));
-    }
-    return arrayOfAttributes;
+    return [...document.getElementsByClassName(className)]
+        .map(el => el.getAttribute(attr).toString());
+}
 
-    function validateRequest() {
-        if (radioCollection.length < 1){
-            throw new Error(`No elements of class '${className}' found.`);
-        }
-        
-        for (let radio of radioCollection) {
-            if (!radio.hasAttribute(attr))
-                throw new Error(`No ${attr} assigned to this element.`);
-        }
+
+function validateRequest(collection) {
+    if (collection.length < 1) {
+        throw new Error(`No elements of class '${className}' found.`);
+    }
+
+    for (let radio of collection) {
+        if (!radio.hasAttribute(attr))
+            throw new Error(`No ${attr} assigned to this element.`);
     }
 }
