07e07150f4c8db64d87bd54994ead3726762b3df
KostasLib <k.liberopoulos@outlook.com>
2018-09-15 00:20:27 +0300

    Added RadioSection class that dynamically loads sections to the DOM.

---
+++ src/js/RadioSection.js
@@ -0,0 +1,30 @@
+class RadioSection {
+    constructor(category) {
+        this.render(category);
+    }
+
+    render(category) {
+        // Creates a <h2> heading.
+        const h2 = document.createElement('h2');
+        const type = category[0].toString().toUpperCase() + category.slice(1);
+        h2.innerHTML = `${type} Radios`;
+
+        // Creates a <ul> that holds the radios.
+        const ul = document.createElement('ul');
+        ul.setAttribute('id', `${category}-radios`);
+        ul.setAttribute('class', 'radio-item-list');
+
+        // Creates a <section> that holds both the heading and the list.
+        const section = document.createElement('section');
+        section.setAttribute('id', `${category}-radios-section`);
+        section.setAttribute('class', 'section-row');
+        section.appendChild(h2);
+        section.appendChild(ul);
+
+        // Appends the section to the parent <div> container.
+        document.getElementById('radio-sections-container')
+            .appendChild(section);
+    }
+}
+
+export default RadioSection;
