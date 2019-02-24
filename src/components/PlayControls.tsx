9a983089418ee8eef091fcb305b72cfd863adcc7
Kostas Liberopoulos <libekonst@gmail.com>
2019-02-24 07:04:35 +0200

    Move PlayControls to new component.

---
+++ src/components/PlayControls.tsx
@@ -0,0 +1,6 @@
+import { MdPlayArrow, MdPause } from 'react-icons/md';
+import React from 'react';
+import './PlayControls.scss';
+
+export const PlayIcon = () => <MdPlayArrow className="controls" />;
+export const PauseIcon = () => <MdPause className="controls" />;
--- src/components/RadioCardA.tsx
+++ src/components/RadioCardA.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './RadioCardA.scss';
-import { MdPlayArrow } from 'react-icons/md';
+import { PlayIcon, PauseIcon } from './PlayControls';
 
 export class RadioCardA extends React.Component {
   state = {
     hovered: false,
+    active: false,
   };
   onMouseEnter = () => this.setState({ hovered: true });
   onMouseLeave = () => this.setState({ hovered: false });
+  onClick = () => this.setState({ active: !this.state.active });
 
   render() {
     let backdrop = 'cardA__backdrop';
@@ -16,20 +18,30 @@ export class RadioCardA extends React.Component {
     let image = 'cardA__image-container';
     if (this.state.hovered) image += '--blur';
     return (
-      <span style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', }}>
+      <span
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'column',
+        }}
+        >
         <div
           className="cardA"
           onMouseEnter={this.onMouseEnter}
           onMouseLeave={this.onMouseLeave}
-        >
+          onClick={this.onClick}
+          >
           <div className={image}>
             <img
+          
               className="cardA__image"
               src="http://www.music892.gr/wp-content/uploads/2017/09/cropped-MASTER_MUSIC_LOGO-2.png"
             />
           </div>
-          {this.state.hovered && <MdPlayArrow className="cardA__icon" />}
-          <div className={backdrop} ></div>
+          {this.state.hovered && !this.state.active && <PlayIcon />}
+          {this.state.hovered && this.state.active && <PauseIcon />}
+          <div className={backdrop} />
         </div>
         <h3>Music 89.2</h3>
       </span>
