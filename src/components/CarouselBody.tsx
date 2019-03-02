d314067e96cfce89254f6c63d50bb43f54fd4cff
Kostas Liberopoulos <libekonst@gmail.com>
2019-03-02 15:52:51 +0200

    Update scroll behavior and scrollbar styles.

--- src/components/CarouselBody.tsx
+++ src/components/CarouselBody.tsx
@@ -6,17 +6,76 @@ import { IRadio } from '../data';
 interface IProps {
   data: IRadio[];
 }
-export const CarouselBody: React.FunctionComponent<IProps> = ({ data }) => (
-  <HorizontalList>
-    {data.map(r => (
-      <li><CardContainer radio={r} key={r.id} /></li>
-    ))}
-  </HorizontalList>
-);
+// export const CarouselBody: React.FunctionComponent<IProps> = ({ data }) => (
+//   <HorizontalList>
+//     {data.map(r => (
+//       <li>
+//         <CardContainer radio={r} key={r.id} />
+//       </li>
+//     ))}
+//   </HorizontalList>
+// );
 
+interface IState {
+  hovered: boolean;
+}
+export class CarouselBody extends React.Component<IProps, IState> {
+  state = {
+    hovered: false,
+  };
+  handleHover = () => this.setState(prev => ({ hovered: !prev.hovered }));
+
+  render() {
+    const { data } = this.props;
+    return (
+      <HorizontalList
+        onMouseEnter={this.handleHover}
+        onMouseLeave={this.handleHover}
+        scroll={this.state.hovered}
+      >
+        {data.map(r => (
+          <li>
+            <CardContainer radio={r} key={r.id} />
+          </li>
+        ))}
+      </HorizontalList>
+    );
+  }
+}
+
+interface IListProps {
+  scroll: boolean;
+}
 const HorizontalList = styled.ul`
-  overflow-x: scroll;
+  overflow-x: ${({ scroll }: IListProps) => (scroll ? 'scroll' : 'hidden')};
   overflow-y: hidden;
   display: flex;
   background-color: none;
+
+  &::-webkit-scrollbar {
+    height: 9px;
+  }
+
+  &::-webkit-scrollbar-track {
+    border-radius: 1rem;
+    background: rgba(0, 0, 0, 0.1);
+
+    &:hover {
+      border: 1px solid #ccc;
+    }
+  }
+
+  &::-webkit-scrollbar-thumb {
+    border-radius: 10px;
+    background: rgba(30, 30, 30, 0.2);
+
+    &:hover {
+      background: rgba(30, 30, 30, 0.3);
+      height: 20px;
+    }
+
+    &:active {
+      background: rgba(30, 30, 30, 0.4);
+    }
+  }
 `;
