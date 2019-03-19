4193675aba2284bcf5821ac0d798486238a87fd7
Kostas Liberopoulos <libekonst@gmail.com>
2019-03-19 16:56:01 +0200

    Add text ellipsis.

--- src/components/card/Title.tsx
+++ src/components/card/Title.tsx
@@ -1,7 +1,13 @@
 import styled from 'styled-components';
 
 export const Title = styled.p`
-  display: block;
+  /* Break word
   overflow-wrap: break-word;
-  white-space: normal;
+  white-space: normal; */
+
+  /* Ellipsis */
+  width: 10rem; /* Need a fixed width for ellipsis to work. */
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
 `;
