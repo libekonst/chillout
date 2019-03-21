dd4917a8f10e58956b7a4a5928017ab7fa182e2d
Kostas Liberopoulos <libekonst@gmail.com>
2019-03-21 03:03:07 +0200

    Prevent FOUC by rendering  after the window loads.

--- src/App.tsx
+++ src/App.tsx
@@ -9,35 +9,53 @@ import { GridHeader } from './components/grid/GridHeader';
 
 class App extends Component {
   state = {
-    data: data,
     renderCarousel: true,
+    // @ts-ignore
+    loaded: false,
   };
+  componentDidMount() {
+    window.addEventListener('load', () => this.setState({ loaded: true }));
+  }
   renderGrid = () => {
-    return data.map(item => (
-      <li key={item.id}>
-        <GridBodyRow name={item.name} image={item.image} label={item.label} />
-      </li>
-    ));
+    return (
+      <ul>
+        {data.map(item => (
+          <li key={item.id}>
+            <GridBodyRow name={item.name} image={item.image} label={item.label} />
+          </li>
+        ))}
+      </ul>
+    );
   };
   // @ts-ignore
   showCarousel = () => this.setState(prev => ({ renderCarousel: !prev.renderCarousel }));
   render() {
     return (
-      <main style={{ paddingLeft: '20px', paddingRight: '20px' }}>
-        <button
-          style={{
-            width: '100px',
-            height: '50px',
-            backgroundColor: 'blue',
-            color: 'pink',
-          }}
-          onClick={this.showCarousel}
-        >
-          {this.state.renderCarousel ? 'Hide Carousel' : 'Show Carousel'}
-        </button>
-        {this.state.renderCarousel && <Carousel data={data} step={7} />}
-        <GridHeader />
-        <ul>{this.renderGrid()}</ul>
+      <main
+        style={{
+          paddingLeft: '20px',
+          paddingRight: '20px',
+          visibility: this.state.loaded ? 'visible' : 'hidden',
+        }}
+      >
+        {this.state.loaded && (
+          <>
+            <button
+              style={{
+                width: '100px',
+                height: '50px',
+                backgroundColor: 'blue',
+                color: 'pink',
+              }}
+              onClick={this.showCarousel}
+            >
+              {this.state.renderCarousel ? 'Hide Carousel' : 'Show Carousel'}
+            </button>
+            {this.state.renderCarousel && <Carousel data={data} step={7} />}
+            <GridHeader />
+            {this.renderGrid()}
+          </>
+        )}
       </main>
     );
   }
