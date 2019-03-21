76bd0aa5c237e98bedcdbad40a3ceecefdff4e2b
Kostas Liberopoulos <libekonst@gmail.com>
2019-03-21 21:51:01 +0200

    Update loader.

--- src/loader/Loader.tsx
+++ src/loader/Loader.tsx
@@ -1,11 +1,12 @@
 import React, { FC } from 'react';
 import './Loader.css';
+import './spinner.css';
 
 interface IProps {
   hide: boolean;
 }
 export const Loader: FC<IProps> = props => (
-  <div className={`loaderWrapper ${props.hide && 'loaderWrapper--hide'}`}>
-    <div className="loader" />
+  <div className={`loader-wrapper ${props.hide && 'loader-wrapper--hide'}`}>
+    <div className="spinner" />
   </div>
 );
