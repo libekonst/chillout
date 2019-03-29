39e9bb4a0da2ce029e4745c53c52e13c06d64101
Kostas Liberopoulos <libekonst@gmail.com>
2019-03-29 14:44:33 +0200

    Update theme.ts

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,7 +1,10 @@
 const theme: any = {
   colors: {
     blue: '#052fb8',
-    purple: 'rgb(255, 32, 117)',
+    // purple: 'rgb(255, 32, 117)',
+    // purple: '#d57eeb',
+    purple: '#f68084',
+    // purple: '#fccb90',
   },
 };
 
