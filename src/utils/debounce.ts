f003d14a41eef25363353787f2ee0de7468ede49
Kostas Liberopoulos <libekonst@gmail.com>
2019-04-07 20:44:10 +0300

    Update debounce.ts

--- src/utils/debounce.ts
+++ src/utils/debounce.ts
@@ -1,13 +1,22 @@
-/** 
+/**
  * Deploy some closure magic to call a function only after a certain timespan has elapsed without calling it again.
- * Useful to suspend subsequent setState calls caused by user events and only execute the latest call. 
+ *
+ * This high order function takes a `callback` as an argument and returns a function that can be safely called repeatedly.
+ * The debounced function wil not invoke the `callback` until it stops being called for `delay` ms.
+ *
+ * When invoked, the debounced function immediately cancels the timeout established by the preceding call, preventing the
+ * `callback` from reaching the Callback Queue. The timeoutID variable holds the id of the timeout established by setTimeout.
+ * Closure allows the returned inner function to have access to timeoutID, which is utilized to clear the previous timeout and
+ * reassign the variable to the latest timeout id.
+ *
+ * Useful for suspending subsequent setState calls caused by user events and only execute the latest call.
  */
-const debounce = (fn: () => any, delay = 250): (() => void) => {
+const debounce = (callback: () => any, delay = 250): (() => void) => {
   let timeoutID: number;
 
-  return (): void => {
+  return () => {
     clearTimeout(timeoutID);
-    timeoutID = setTimeout(fn, delay);
+    timeoutID = setTimeout(callback, delay);
   };
 };
 
