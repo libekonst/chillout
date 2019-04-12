03b69d521f1f35fe714616f899c75472e79e31b0
Konstantinos Liberopoulos <libekonst@gmail.com>
2019-04-12 05:23:21 +0300

    Update setDocTitle.ts

--- src/utils/setDocTitle.ts
+++ src/utils/setDocTitle.ts
@@ -1,6 +1,6 @@
 /** Changes the document.title property or resets it if no text is provided, and returns the new title. */
 const setDocTitle = (text?: string): string => {
-  if (text) return (document.title = text);
+  if (text) return (document.title = `${text} - The Chillout App`);
   return (document.title = 'The Chillout App');
 };
 export default setDocTitle;
