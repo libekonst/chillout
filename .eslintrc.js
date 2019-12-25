6125c93355b6c9f8b14880e3268b06f3558a3a86
Kostas Liberopoulos <libekonst@gmail.com>
2019-12-26 01:21:23 +0200

    Update packages.

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  extends: [
+    'airbnb-typescript',
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'prettier',
+    'prettier/react',
+    // 'plugin:jest/recommended'
+  ],
+  plugins: ['react-hooks'],
+  rules: {
+    'react/jsx-fragments': [1, 'syntax'],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'import/prefer-default-export': 0,
+    'react/prop-types': 0
+  },
+  parserOptions: {
+    jsx: true,
+    useJSXTextNode: true
+  }
+};
--- src/views/HomeView.tsx
+++ src/views/HomeView.tsx
@@ -1,36 +1,36 @@
 import React, { FC } from 'react';
 import styled from 'styled-components';
 
-interface IProps {
-  footer?: JSX.Element;
-  sidebar?: JSX.Element;
-  mainContent?: JSX.Element;
+interface Props {
+	footer?: JSX.Element;
+	sidebar?: JSX.Element;
+	mainContent?: JSX.Element;
 }
-export const HomeView: FC<IProps> = ({ footer, sidebar, mainContent }) => (
-  <>
-    {sidebar && <Sidebar>{sidebar}</Sidebar>}
-    {mainContent && <Main>{mainContent}</Main>}
-    {footer && <Footer>{footer}</Footer>}
-  </>
+export const HomeView: FC<Props> = ({ footer, sidebar, mainContent }) => (
+	<>
+		{sidebar && <Sidebar>{sidebar}</Sidebar>}
+		{mainContent && <Main>{mainContent}</Main>}
+		{footer && <Footer>{footer}</Footer>}
+	</>
 );
 
 const Footer = styled.div`
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  right: 0;
+	position: fixed;
+	bottom: 0;
+	left: 0;
+	right: 0;
 `;
 
 const Sidebar = styled.div`
-  position: fixed;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  width: 350px;
+	position: fixed;
+	top: 0;
+	bottom: 0;
+	left: 0;
+	width: 350px;
 `;
 
 const Main = styled.div`
-  height: 100%;
-  width: 100%;
-  padding-left: 350px;
+	height: 100%;
+	width: 100%;
+	padding-left: 350px;
 `;
