99133d3270c63e72b4cab53a1e72a6565f2e5f16
Konstantinos Liberopoulos <libekonst@gmail.com>
2020-01-14 11:20:10 +0200

    Update Audio.service.ts

--- src/services/Audio.service.ts
+++ src/services/Audio.service.ts
@@ -2,4 +2,14 @@
 
 export class AudioService {
 	private readonly _audio = new Audio();
+
+	play(url: string) {
+		this._audio.src = url;
+		this._audio.play();
+	}
+
+	stop() {
+		this._audio.pause();
+		this._audio.src = null;
+	}
 }
